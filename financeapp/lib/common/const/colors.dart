4c010059ce482467295f8ebe5fba63ae3dd80aaf
Moraes Ian <moraesian2007@gmail.com>
2026-05-17 23:44:12 -0300

    .

---
+++ financeapp/lib/common/const/colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class AppColors {
+  // Cores principais
+  static const Color dark_background = Color(0xFF1F2133);
+
+}
---
+++ financeapp/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:financeapp/features/login/login_screem.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+
+  runApp(const FinanceApp());
+}
+
+class FinanceApp extends StatelessWidget {
+  const FinanceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Finance App',
+      debugShowCheckedModeBanner: false,
+      home: const LoginScreem(),
+    );
+  }
+}
